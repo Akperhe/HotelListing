d7a58d0691d53b56d98657969e5b1da2a7e5b5c1
OGHENEOVO.AKPERHE <OGHENEOVO.AKPERHE@WEMA-CTI-551>
2022-02-10 16:46:18 +0100

    Add project files.

---
+++ HotelListing/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelListing.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet(Name = "GetInt")]
+        public int Get(int id)
+        {
+            return id;
+        }
+    }
+}
